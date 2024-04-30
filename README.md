85e22afb591d3a0f74b1836d077962ee5280828f
tf63 <74246282+tf63@users.noreply.github.com>
2024-04-30 18:29:41 +0900

    change docker env

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-## Transformer
+# PyTorch環境
+
+ビルドは`docker.sh`参照
+```
+    bash docker.sh [build|shell|root|help]
+```
--- docker.sh
+++ docker.sh
@@ -10,24 +10,24 @@
 DATASET_DIRS="$HOME/dataset"
 DATA_DIRS="$HOME/data"
 
-DOCKERFILE_NAME="Dockerfile.cu117"
+DOCKERFILE_NAME="Dockerfile.cu171"
 TORCH_VERSION="torch-2.0.0"
 # cu111 - torch-1.9.0
 # cu117 - torch-1.13.0, torch-2.0.0
 
 build()
 {
-    docker build . -f docker/$DOCKERFILE_NAME --target $TORCH_VERSION --build-arg USER_UID=`(id -u)` --build-arg USER_GID=`(id -g)` -t $TORCH_VERSION:poetry
+    docker build . -f docker/$DOCKERFILE_NAME --target $TORCH_VERSION --build-arg USER_UID=`(id -u)` --build-arg USER_GID=`(id -g)` -t $TORCH_VERSION
 }
 
 shell() 
 {
-    docker run --rm --gpus all --shm-size=16g -it -v $(pwd):/app -v $DATASET_DIRS:/dataset -v $DATA_DIRS:/data $TORCH_VERSION:poetry /bin/bash
+    docker run --rm --gpus all --shm-size=16g -it -v $(pwd):/app -v $DATASET_DIRS:/dataset -v $DATA_DIRS:/data $TORCH_VERSION /bin/bash
 }
 
 root()
 {
-    docker run --rm --gpus all --shm-size=16g --user 0:0 -it -v $(pwd):/app -v $DATASET_DIRS:/dataset -v $DATA_DIRS:/data $TORCH_VERSION:poetry /bin/bash
+    docker run --rm --gpus all --shm-size=16g --user 0:0 -it -v $(pwd):/app -v $DATASET_DIRS:/dataset -v $DATA_DIRS:/data $TORCH_VERSION /bin/bash
 }
 
 help()
@@ -37,7 +37,7 @@ help()
 
 
 if [[ $1 == "build" ]]; then
-    build $2
+    build
 elif [[ $1 == "shell" ]]; then
     shell 
 elif [[ $1 == "root" ]]; then
